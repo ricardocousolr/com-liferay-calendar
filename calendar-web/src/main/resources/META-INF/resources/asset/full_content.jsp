marcellustavares/liferay-portal

LPS-69400 update references for Calendar

modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/asset/full_content.jsp
@@ -110,14 +110,14 @@
 			<liferay-ui:message key="location" />: <a href="https://maps.google.com.br/maps?q=<%= HtmlUtil.escapeHREF(calendarBooking.getLocation()) %>" target="_blank"><%= HtmlUtil.escape(calendarBooking.getLocation()) %></a>
 		</c:if>
 
-		<liferay-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
-			<liferay-ui:custom-attribute-list
+		<liferay-custom-field-ui:custom-attribute-available className="<%= CalendarBooking.class.getName() %>">
+			<liferay-custom-field-ui:custom-attribute-list
 				className="<%= CalendarBooking.class.getName() %>"
 				classPK="<%= (calendarBooking != null) ? calendarBooking.getCalendarBookingId() : 0 %>"
 				editable="<%= false %>"
 				label="<%= true %>"
 			/>
-		</liferay-ui:custom-attributes-available>
+		</liferay-custom-field-ui:custom-attribute-available>
 	</p>
 </div>
 

modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/edit_calendar_booking.jsp
@@ -346,14 +346,14 @@ while (manageableCalendarsIterator.hasNext()) {
 
 					<aui:input name="location" />
 
-					<liferay-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
-						<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-available className="<%= CalendarBooking.class.getName() %>">
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= CalendarBooking.class.getName() %>"
 							classPK="<%= (calendarBooking != null) ? calendarBooking.getCalendarBookingId() : 0 %>"
 							editable="<%= true %>"
 							label="<%= true %>"
 						/>
-					</liferay-ui:custom-attributes-available>
+					</liferay-custom-field-ui:custom-attribute-available>
 
 					<c:if test="<%= calendarBooking == null %>">
 						<aui:field-wrapper label="permissions">

modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/view_calendar_booking.jsp
@@ -104,14 +104,14 @@ AssetEntry layoutAssetEntry = AssetEntryLocalServiceUtil.getEntry(CalendarBookin
 				</c:if>
 			</dl>
 
-			<liferay-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attribute-available className="<%= CalendarBooking.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= CalendarBooking.class.getName() %>"
 					classPK="<%= calendarBooking.getCalendarBookingId() %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 
 			<p>
 				<%= calendarBooking.getDescription(locale) %>

ricardocousolr/com-liferay-calendar

calendar-web/src/main/resources/META-INF/resources/asset/full_content.jsp
@@ -110,14 +110,14 @@
 			<liferay-ui:message key="location" />: <a href="https://maps.google.com.br/maps?q=<%= HtmlUtil.escapeHREF(calendarBooking.getLocation()) %>" target="_blank"><%= HtmlUtil.escape(calendarBooking.getLocation()) %></a>
 		</c:if>
 
-		<liferay-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
-			<liferay-ui:custom-attribute-list
+		<liferay-custom-field-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
+			<liferay-custom-field-ui:custom-attribute-list
 				className="<%= CalendarBooking.class.getName() %>"
 				classPK="<%= (calendarBooking != null) ? calendarBooking.getCalendarBookingId() : 0 %>"
 				editable="<%= false %>"
 				label="<%= true %>"
 			/>
-		</liferay-ui:custom-attributes-available>
+		</liferay-custom-field-ui:custom-attributes-available>
 	</p>
 </div>
 

calendar-web/src/main/resources/META-INF/resources/edit_calendar_booking.jsp
@@ -346,14 +346,14 @@ while (manageableCalendarsIterator.hasNext()) {
 
 					<aui:input name="location" />
 
-					<liferay-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
-						<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= CalendarBooking.class.getName() %>"
 							classPK="<%= (calendarBooking != null) ? calendarBooking.getCalendarBookingId() : 0 %>"
 							editable="<%= true %>"
 							label="<%= true %>"
 						/>
-					</liferay-ui:custom-attributes-available>
+					</liferay-custom-field-ui:custom-attributes-available>
 
 					<c:if test="<%= calendarBooking == null %>">
 						<aui:field-wrapper label="permissions">

calendar-web/src/main/resources/META-INF/resources/view_calendar_booking.jsp
@@ -110,14 +110,14 @@ AssetEntry layoutAssetEntry = AssetEntryLocalServiceUtil.getEntry(CalendarBookin
 				</c:if>
 			</dl>
 
-			<liferay-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attributes-available className="<%= CalendarBooking.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= CalendarBooking.class.getName() %>"
 					classPK="<%= calendarBooking.getCalendarBookingId() %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 
 			<p>
 				<%= calendarBooking.getDescription(locale) %>